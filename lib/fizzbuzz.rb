d1a42317f1ec532496bcd91cc30d4b339c2da285
delphine <delphine.mubiligi@gmail.com>
2022-06-07 15:52:34 +0100

    added test for numbers that are divisible by 15

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,10 +1,11 @@
 def fizzbuzz(num)
   if num % 3 == 0 && num % 5 == 0
-    return "fizzBuzz"
+    return "fizzbuzz"
   elsif num % 3 == 0
     return "fizz"
   elsif num % 5 == 0
     return "buzz"
+  else 
+    return num
   end
-
 end
