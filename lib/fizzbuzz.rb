dbfc804b2c49f23efe3c77c336abde654ecb646b
delphine <delphine.mubiligi@gmail.com>
2022-06-07 14:43:14 +0100

    added lib folder and fixed method error

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,7 +1,13 @@
-  if num % 3 == 0 && num % 5 == 0
+=begin
+if num % 3 == 0 && num % 5 == 0
     puts “fizzBuzz”
   elsif num % 3 == 0
     puts “fizz”
   elsif num % 5 == 0
     puts “buzz”
   end
+=end
+
+
+def fizzbuzz
+end
